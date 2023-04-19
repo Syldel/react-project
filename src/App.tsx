88b27a816064e3247a13a51829d16bf71a55cbd4
Syl D <sdelescluse@atecna.fr>
2023-04-19 10:21:57 +0200

    chore: install React 18.2.0

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import logoBlack from './images/logo-black.svg'
+
+const App = () => {
+  return <div className="bg-slate-100">
+    <img src={logoBlack} alt="logo" />
+  </div>
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
+/// <reference types="vitest" />
+import path from 'path'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    }),
+  ],
+  test: {
+    include: ['test/**/*.test.{j,t}s'],
+    environment: 'jsdom',
+  },
 })
