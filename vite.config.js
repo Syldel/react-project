c97fccfe485f62907d9c60c4e8454d57e3c3dc21
Syldel <nightfand@hotmail.com>
2025-04-24 22:53:39 +0200

    build(vite): load environment variables using loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
 /// <reference types="vitest" />
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
-    react({
-      include: '**/*.tsx',
-    }),
-  ],
-  test: {
-    include: ['test/**/*.test.{j,t}s'],
-    environment: 'jsdom',
-  },
+    plugins: [
+      react({
+        include: '**/*.tsx',
+      }),
+    ],
+    test: {
+      include: ['test/**/*.test.{j,t}s'],
+      environment: 'jsdom',
+    },
+  }
 })
