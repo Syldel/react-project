4a699aefb8f32bb72ac66d8e6d3f00ab588f2380
Syldel <nightfand@hotmail.com>
2025-04-24 21:12:34 +0000

    Deploying to gh-pages from  @ 63caa3b28c8a7cb7a5ef960d8b002bd95ec0cd09 🚀

---
+++ assets/error404-74c0ad84.js
@@ -0,0 +1,1 @@
+import{j as e,a as r}from"./index-702ca4aa.js";function n(){return e("main",{children:[r("h1",{children:"Erreur 404"}),r("p",{children:"Page introuvable"})]})}export{n as default};
--- assets/error404-fde91822.js
+++
@@ -1,1 +0,0 @@
-import{j as e,a as r}from"./index-7b9233aa.js";function n(){return e("main",{children:[r("h1",{children:"Erreur 404"}),r("p",{children:"Page introuvable"})]})}export{n as default};
