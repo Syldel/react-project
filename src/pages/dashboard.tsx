63caa3b28c8a7cb7a5ef960d8b002bd95ec0cd09
Syldel <nightfand@hotmail.com>
2025-04-24 23:12:08 +0200

    feat(dashboard): add new links to the dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,26 @@
+import { Link } from 'react-router-dom'
+
 function Dashboard() {
   return (<main>
-    <h1>
-      Trophées
+    <h1 className="text-3xl font-semibold text-center text-gray-700 uppercase">
+      Dashboard
     </h1>
 
-    <p>
-      WIP
-    </p>
+    <br />
+
+    <div className="flex flex-col gap-4">
+      <Link
+        className="text-blue-500 hover:underline"
+        to="/login">
+        Se connecter
+      </Link>
+
+      <Link
+        className="text-blue-500 hover:underline"
+        to="/register">
+        S’inscrire
+      </Link>
+    </div>
   </main>
   )
 }
